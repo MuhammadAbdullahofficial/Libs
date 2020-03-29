5cc46f196bf4374d5137b3dd7361420cb89c1c8c
Muhammad Abdullah <mabdullahiqbal.official@gmail.com>
2020-03-29 17:52:46 +0500

    Adding Environment parameter to main function
    https://docs.microsoft.com/en-us/cpp/cpp/main-function-command-line-args?view=vs-2019

--- cpp/hello_world.cpp
+++ cpp/hello_world.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+int main(int argc, char* argv[], char* envp[]) {
 
-int main(int argc, char* argv[]) {
+	std::cout << "argc : " << argc;
 
-	std::cout << "argc : " << argc << std::endl;
-	std::cout << "argv : ";
+	std::cout << "\nargv : ";
 	for (size_t i = 0; i < argc; i++)
 		std::cout << argv[i] << ' ';
+
+	std::cout << "\nenvp : ";
+	for (size_t i = 0; envp[i] != NULL; i++)
+		std::cout << envp[i] << '\n';
 
 	std::cin.get();
 	return 0;
