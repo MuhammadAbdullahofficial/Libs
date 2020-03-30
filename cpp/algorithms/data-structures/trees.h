cae680eec1cf8f5ecd95f02c29d99a5908f72419
Muhammad Abdullah <mabdullahiqbal.official@gmail.com>
2020-03-30 17:50:03 +0500

    Update trees.h

--- cpp/algorithms/data-structures/trees.h
+++ cpp/algorithms/data-structures/trees.h
@@ -10,6 +10,7 @@ public:
 	virtual void add(t value);
 	virtual void remove(t value);
 	virtual bool exist(t value);
+	virtual void clear();
 	virtual int size()const;
 };
 
@@ -17,11 +18,18 @@ template<class t>
 class BinaryTree : public Tree<t>
 {
 public:
+
 	BinaryTree() :root(null), nodes(0) {}
+	BinaryTree(std::initializer_list<t> init) :root(null), nodes(0) { for (t node : init) { this->add(node); } }
+	void operator=(std::initializer_list<t> list) { clear(); for (t node : list) { this->add(node); } }
+	void operator+=(std::initializer_list<t> list) { for (t node : list) { this->add(node); } };
+	void operator+=(BinaryTree<t>& tree) {  };
+
 
 	void add(t value) {
 		if (!root) {
 			root = new DoubleLinkNode<t>(value);
+			nodes += 1;
 		}
 		else
 		{
@@ -33,6 +41,7 @@ public:
 					if (tra->p_link == null)
 					{
 						tra->p_link = new DoubleLinkNode<t>(value);
+						nodes += 1;
 						return;
 					}
 					else
@@ -42,6 +51,7 @@ public:
 					if (tra->n_link == null)
 					{
 						tra->n_link = new DoubleLinkNode<t>(value);
+						nodes += 1;
 						return;
 					}
 					else
@@ -53,30 +63,46 @@ public:
 	void remove(t value) {
 		
 	}
-	bool exist(t value) { return false; }
-	int size()const { return 0; }
+	bool exist(t value) { return get_node_internal(root, value) == null ? false : true; }
+	int size()const { return nodes; }
+	void clear() { clear_internal(root); root = null; nodes = 0; }
+	~BinaryTree(){ clear(); }
 
-	~BinaryTree()
-	{
-	}
-
-	//---------------------------------------
-	//for validation temp               //*--
-	void p(DoubleLinkNode<t>* n) {      //*--
-		if (n == null)return;			//*--
-		p(n->p_link);					//*--
-		std::cout << n->data << ' ';	//*--
-		p(n->n_link);					//*--
-	}									//*--
-										//*--
-	void print() {						//*--
-		p(root);						//*--
-	}									//*--
-	//---------------------------------------
+	/**///---------------------------------------///**/temparary validation code
+	/**/void print(DoubleLinkNode<t>* n) {  	 ///**/
+	/**/	if (n == null)return;				 ///**/
+	/**/	print(n->p_link);					 ///**/
+	/**/	std::cout << n->data << ' ';		 ///**/
+	/**/	print(n->n_link);					 ///**/
+	/**/}										 ///**/
+	/**/void print() {							 ///**/
+	/**/	print(root);						 ///**/
+	/**/}										 ///**/
+	/**///---------------------------------------///**/
 
 private:
 	DoubleLinkNode<t>* root;
 	unsigned int nodes;
+
+	void clear_internal(DoubleLinkNode<t>* node) {
+		if (node == null)return;
+		clear_internal(node->p_link);
+		DoubleLinkNode<t>* node_n = node->n_link;
+		delete node;
+		node = null;
+		clear_internal(node_n);
+	}
+
+	DoubleLinkNode<t>* get_node_internal(DoubleLinkNode<t>* node, t value) {
+		if (node == null)return null;
+		else if (node->data == value)return node;
+		else if (node->data > value)return get_node_internal(node->p_link, value);
+		else if (node->data < value)return get_node_internal(node->n_link, value);
+		else return null;
+	}
+
+	
+
 };
 
 #endif // !TREES_F
