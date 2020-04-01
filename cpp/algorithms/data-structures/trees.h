81add7c44a36a257b29410f0459560454f54fce6
Muhammad Abdullah <mabdullahiqbal.official@gmail.com>
2020-04-01 23:37:46 +0500

    Update trees.h

--- cpp/algorithms/data-structures/trees.h
+++ cpp/algorithms/data-structures/trees.h
@@ -8,7 +8,7 @@ __interface Tree
 {
 public:
 	virtual void add(t value);
-	virtual void remove(t value);
+	//virtual void remove(t value);
 	virtual bool exist(t value);
 	virtual void clear();
 	virtual int size()const;
@@ -61,9 +61,8 @@ public:
 			}
 		}
 	}
-	void remove(t value) {
-		
-	}
+
+	
 	bool exist(t value) { return get_node_internal(root, value) == null ? false : true; }
 	int size()const { return nodes; }
 	void clear() { clear_internal(root); root = null; nodes = 0; }
@@ -102,6 +101,15 @@ private:
 		else return null;
 	}
 
+	DoubleLinkNode<t>* get_node_parent_internal(DoubleLinkNode<t>* node, t value) {
+		if (node == null)return null;
+		else if (node->n_link->data == value || 
+			node->p_link->data == value)return node;
+		else if (node->data > value)return get_node_parent_internal(node->p_link, value);
+		else if (node->data < value)return get_node_parent_internal(node->n_link, value);
+		else return null;
+	}
+
 	void merge_internal(DoubleLinkNode<t>* node) {
 		if (node == null)return;
 		else {
@@ -109,6 +117,21 @@ private:
 			this->add(node->data);
 			merge_internal(node->n_link);
 		}
+	}
+
+	void connect_subtree_internal(DoubleLinkNode<t>* main, DoubleLinkNode<t> * sub) {
+		if (main == null) { return; }
+		else if (main->data == sub->data) { return; }
+		else if (main->data > sub->data&& main->p_link != null) { connect_subtree_internal(main->p_link, sub); }
+		else if (main->data > sub->data&& main->p_link == null) { main->p_link = sub; nodes += subtree_size_internal(sub);  return; }
+		else if (main->data < sub->data&& main->n_link != null) { connect_subtree_internal(main->n_link, sub);}
+		else if (main->data < sub->data&& main->n_link == null) { main->n_link = sub; nodes += subtree_size_internal(sub);  return; }
+	}
+
+	int subtree_size_internal(DoubleLinkNode<t>* node) {
+		if (node == null)return 0;
+		else if (node->n_link == null && node->p_link == null)return 1;
+		else return 1 + subtree_size_internal(node->p_link) + subtree_size_internal(node->n_link);
 	}
 
 };
