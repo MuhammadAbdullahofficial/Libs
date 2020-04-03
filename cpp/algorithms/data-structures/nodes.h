da3eb03f43e165f3c13f4372e2bc69414b4d3415
Muhammad Abdullah <mabdullahiqbal.official@gmail.com>
2020-04-03 18:04:16 +0500

    Update nodes.h

--- cpp/algorithms/data-structures/nodes.h
+++ cpp/algorithms/data-structures/nodes.h
@@ -29,4 +29,48 @@ struct MultiLinkNode
 	MultiLinkNode(t val, MultiLinkNode** lnks, unsigned int size) :data(val), links(lnks), links_size(size) {}
 };
 
+struct SingleLinkNode_g
+{
+	SingleLinkNode_g* link;	
+	virtual ~SingleLinkNode_g() {}
+};
+
+struct DoubleLinkNode_g
+{
+	DoubleLinkNode_g* n_link;
+	DoubleLinkNode_g* p_link;
+	virtual ~DoubleLinkNode_g() {}
+};
+
+struct MultiLinkNode_g
+{
+	unsigned int links_size;
+	MultiLinkNode_g** links;
+	virtual ~MultiLinkNode_g() {}
+};
+
+template<class t>
+struct SingleDataNode : public SingleLinkNode_g
+{
+	t data;
+	SingleDataNode(t val) :data(val) { link = null; }
+	SingleDataNode(t val, SingleLinkNode_g* lnk) :data(val) { link = lnk; }
+};
+
+template<class t>
+struct DoubleDataNode : public DoubleLinkNode_g
+{
+	t data;
+	DoubleDataNode(t val) :data(val) { p_link = n_link = null; }
+	DoubleDataNode(t val, DoubleLinkNode_g* n_lnk, DoubleLinkNode_g* p_lnk) :data(val) { n_link = n_lnk; p_link = p_lnk; }
+};
+
+template<class t>
+struct MultiDataNode : public MultiLinkNode_g
+{
+	t data;
+	MultiDataNode(t val) :data(val) { links = null; }
+	MultiDataNode(t val, MultiLinkNode_g** lnks) :data(val) { links = lnks; }
+};
+
 #endif // !NODES_F
